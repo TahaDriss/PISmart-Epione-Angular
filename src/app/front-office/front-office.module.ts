5c9bed9f24ada52f9dc4d8125017f08066c1100d
TahaDriss <taha.driss@esprit.tn>
2018-12-26 16:15:51 +0100

    First Commit

---
+++ src/app/front-office/front-office.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccueilComponent } from './accueil/accueil.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path : 'ac', component : AccueilComponent },
+];
+@NgModule({
+  declarations: [AccueilComponent],
+  imports: [
+    CommonModule,  RouterModule.forChild(routes)
+  ]
+})
+export class FrontOfficeModule { }
